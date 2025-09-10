bdac30deb7e64552e55ef6d3376a731dce0868e9
Rodrigo <soguiso@gmail.com>
2025-09-10 00:23:44 -0300

    Refactor Azure provider configuration for consistency and clarity

--- provider.tf
+++ provider.tf
@@ -1,18 +1,13 @@
 provider "azurerm" {
-  features {
-  }
+  features {}
+  
 }
 
-
 terraform {
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 3.0"
     }
   }
-
-  required_version = ">= 1.0"
-
 }
