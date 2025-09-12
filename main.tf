2d59929109cda2b05118dc1b4c6941a23a9912a5
Rodrigo <soguiso@gmail.com>
2025-09-12 15:24:18 -0300

    Refactor resource group configuration to use variable for name

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "devops-automation-rg"
-  location = "eastus"
-
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "devops-automation-rg"
+}
+
+variable "admin_username" {
+      default = "azureuser"
+    }
+
+variable "admin_password" {
+      default = "SenhaForte1234!"
+    }
